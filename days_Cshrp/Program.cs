5eece76222edccd60e632ee9be70aeb3e05cd66c
NikolaiMarkalainen <nikolai.markalainen@gmail.com>
2023-04-08 20:56:39 +0300

    Sys.commandline added for calendar

--- days_Cshrp/Program.cs
+++ days_Cshrp/Program.cs
@@ -1,30 +1,100 @@
-﻿using System;
-namespace SaveDataToCSV;
+﻿using System.CommandLine;
+
 class Program
 {
-    static void Main(string[] args)
+    static async Task<int> Main(string[] args)
     {
-        Console.WriteLine("Enter your firstname: ");
-        string? fname = Console.ReadLine();
-        Console.WriteLine("Enter your lastname");
-        string? lname = Console.ReadLine();
-        Console.WriteLine("Enter your age");
-        string? ageInput = Console.ReadLine();
-        int age;    
+        var fileOption = new Option<FileInfo?>(
+            name: "list",
+            description: "Lists the contents of the csv.",
+            isDefault: true,
+            parseArgument: result =>
+            {
+                if(result.Tokens.Count == 0)
+                {
+                    return new FileInfo("Events.csv");
+                }
+                string? filePath = result.Tokens.Single().Value;
+                if(!File.Exists(filePath))
+                {
+                    result.ErrorMessage = "File does not exsist";
+                    return null;
+                }
+                else
+                {
+                    return new FileInfo(filePath);
+                }
+            });
+
+        var todayOption = new Option<DateTime>(
+            name: "--today",
+            description: "Lists all events that are marked today.");
         
-        if(!int.TryParse(ageInput, out age))
-        {
-            throw new ArgumentException("Invalid input. Please enter a valid number");
-        }
-        if(string.IsNullOrEmpty(fname) || string.IsNullOrEmpty(lname))
-        {
-            throw new ArgumentException("Name is not in correct format");
-        }
+        var beforeOption = new Option<DateTime>(
+            name: "--before",
+            description: "Lists all events prior to today.");
+        
+        var afterOption = new Option<DateTime>(
+           name: "--after",
+           description: "List all events in the future." );
 
-        Save test = new Save(fname, lname, age);
-        Console.WriteLine(test);
-        Save.PromptToSaveToCSV(test);
-        Save.PromptToReadFromCsv();
+        var dateArgument = new Argument<DateTime>(
+            name: "--date",
+            description: "An event to set a calendar appointment");
+        
+        var categoryArgument = new Argument<String>(
+            name: "--category",
+            description: "A description field for calendar appointments");
+
+        var descriptionArgument = new Argument<String>(
+            name: "--description",
+            description: "A way to add a description to the calendar");
+
+
+        var rootCommand = new RootCommand("Sample app for System.CommandLine");
+        rootCommand.AddGlobalOption(fileOption);
+
+        var DaysCommand = new Command("days", "Initial command to start file")
+        {
+            fileOption,
+            todayOption,
+            beforeOption,
+            afterOption
+        };
+
+        var AddCommand = new Command("add", "Add an entry to the file");
+        AddCommand.AddArgument(dateArgument);
+        AddCommand.AddArgument(categoryArgument);
+        AddCommand.AddArgument(descriptionArgument);
+
+        DaysCommand.SetHandler((file, today, before, after) =>
+        {
+            ReadFile(file!, today, before, after);
+        }, fileOption, todayOption, beforeOption, afterOption);
+
+        AddCommand.SetHandler((file, date, category, description) =>
+        {
+            AddEvent(file!, date, category, description);
+        }, fileOption, dateArgument, categoryArgument, descriptionArgument);
+
+        return await rootCommand.InvokeAsync(args);
     }
-}
+        internal static void ReadFile(FileInfo file, DateTime today, DateTime before, DateTime after)
+        {
+            var lines = File.ReadLines(file.FullName).ToList();
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+        }
 
+        internal static void AddEvent(FileInfo file, DateTime date, string category, string description)
+        {
+            Console.WriteLine("Adding to file");
+            using StreamWriter? writer = file.AppendText();
+            writer.WriteLine($"{Environment.NewLine}{Environment.NewLine}{date}");
+            writer.WriteLine($"{Environment.NewLine}{category}");
+            writer.WriteLine($"{Environment.NewLine}{description}");
+            writer.Flush();
+        }
+}
